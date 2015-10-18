b58295bd981c6e7dc011d6164f16671507e9fd61
Jan-Niklas Schneider <heisse.cola@gmail.com>
2015-10-18 21:53:52 +0200

    gcd: Gutenberg small working

---
+++ gcd/week5/Gutenberg/wordcount.py
@@ -0,0 +1,39 @@
+import mincemeat
+import glob
+
+
+all_files = glob.glob("GutenbergSF/*.*")
+def file_contents(file_name):
+    f = open(file_name, encoding="Latin-1")
+    try:
+        return f.read()
+    finally:
+        f.close()
+datasource = dict((file_name, file_contents(file_name)) for file_name in all_files)
+
+def mapfn(k, v):
+    for w in v.split():
+        yield w, 1
+
+def reducefn(k, vs):
+    result = 0
+    for v in vs:
+        result += v
+    return result
+	
+s = mincemeat.Server()
+
+s.datasource = datasource
+s.mapfn = mapfn
+s.reducefn = reducefn
+
+results = s.run_server(password="changeme")
+#sort results from high to low
+results = sorted(results.items(), key=lambda x: x[-1], reverse=True)
+#print results
+i = open('outfile.txt','w')
+i.write(str(results))
+i.close()
+
+
+print(results)
